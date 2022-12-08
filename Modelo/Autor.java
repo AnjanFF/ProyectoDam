9dece507a082ea4e1fb442792bc6f88aff44cb78
AnjanFF <52600828+AnjanFF@users.noreply.github.com>
2022-12-08 14:46:09 +0100

    Added Models
    Added moodels for Autor, Colection, comics, status and table

---
+++ Modelo/Autor.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Angel Flores
+ * 
+ */
+public class Autor implements Serializable{
+    private int idAutor;
+    private String nombre;
+    private Date fechaNac;
+    private byte[] foto;
+    private String urlFoto;
+
+    public Autor() {
+    }
+
+    public Autor(int idAutor, String nombre, Date fechaNac, byte[] foto, String urlFoto) {
+        this.idAutor = idAutor;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.foto = foto;
+        this.urlFoto = urlFoto;
+    }
+
+    public Autor(String nombre, Date fechaNac, byte[] foto, String urlFoto) {
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.foto = foto;
+        this.urlFoto = urlFoto;
+    }
+
+    
+
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+}
---
+++ Modelo/Estado.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Angel Flores
+ * 
+ */
+public class Estado implements Serializable{
+    private int idEstado;
+    private String estado;
+
+    public Estado() {
+    }
+
+    public Estado(int idEstado, String estado) {
+        this.idEstado = idEstado;
+        this.estado = estado;
+    }
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return estado;
+    }
+    
+    
+    
+    
+}
